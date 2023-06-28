f7b39a6daba165b1b21a003c7f3443335bf3c042
Abdul-Mueed-Shahbaz <bscs18056@itu.edu.pk>
2023-06-29 04:34:59 +0500

    bootstrap setup and inital changes

---
+++ secure_channel_hub_gui/src/components/LoginView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-7">
+        <div class="card bg-dark text-white" style="border-radius: 1rem">
+          <div class="card-body p-5 text-center">
+            <form @submit.prevent="" class="mb-md-5 mt-md-4 pb-5">
+              <h2 class="font-weight-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">
+                Please enter your login and password!
+              </p>
+
+              <div class="form-outline form-white mb-4">
+                <input
+                  type="email"
+                  id="typeEmailX"
+                  class="form-control form-control-lg bg-dark text-light form-input"
+                  placeholder="Email"
+                />
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input
+                  type="password"
+                  id="typePasswordX"
+                  class="form-control form-control-lg bg-dark text-light form-input"
+                  placeholder="Password"
+                />
+              </div>
+
+              <p class="small mb-5 pb-lg-2">
+                <a class="text-white-50" href="#!">Forgot password?</a>
+              </p>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">
+                Login
+              </button>
+
+              <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                <a href="#!" class="text-white"
+                  ><i class="fab fa-facebook-f fa-lg"></i
+                ></a>
+                <a href="#!" class="text-white"
+                  ><i class="fab fa-twitter fa-lg mx-4 px-2"></i
+                ></a>
+                <a href="#!" class="text-white"
+                  ><i class="fab fa-google fa-lg"></i
+                ></a>
+              </div>
+            </form>
+
+            <div class="mb-0">
+              Don't have an account?
+              <div
+                class="text-white-50 fw-bold cursor-pointer"
+                @click="$emit('changeStep', 0)"
+              >
+                Sign Up
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- secure_channel_hub_gui/src/views/HomeView.vue
+++ secure_channel_hub_gui/src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="vh-100 gradient-custom">
+    <SignUpView v-if="authStep === 0" @changeStep="changeStep" />
+    <LoginView v-else-if="authStep === 1" @changeStep="changeStep" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script setup>
+import LoginView from "@/components/LoginView.vue";
+import SignUpView from "@/components/SignUpView.vue";
+import { ref } from "vue";
 
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+const authStep = ref(1);
+const changeStep = (e) => {
+  authStep.value = e;
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.gradient-custom {
+  /* fallback for old browsers */
+  background: #6a11cb;
+
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(
+    to right,
+    rgba(106, 17, 203, 1),
+    rgba(37, 117, 252, 1)
+  );
+
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(
+    to right,
+    rgba(106, 17, 203, 1),
+    rgba(37, 117, 252, 1)
+  );
+}
+.form-input::placeholder {
+  color: white;
+  opacity: 1; /* Firefox */
+}
+.cursor-pointer {
+  cursor: pointer;
+}
+</style>
